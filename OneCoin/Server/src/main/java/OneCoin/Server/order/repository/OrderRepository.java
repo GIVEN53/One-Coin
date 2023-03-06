fc0bf07227a5c8a5dce8f78768fd46b52ba6e955
NAM GIBEOM <rlqja0523@naver.com>
2023-03-06 14:11:55 +0900

    ♻️refactor: 타입 변경

--- OneCoin/Server/src/main/java/OneCoin/Server/order/repository/OrderRepository.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/order/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package OneCoin.Server.order.repository;
 
 import OneCoin.Server.order.entity.Order;
+import OneCoin.Server.order.entity.enums.TransactionType;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.List;
 public interface OrderRepository extends CrudRepository<Order, Long> {
     List<Order> findAllByUserId(Long userId);
 
-    List<Order> findAllByOrderTypeAndCode(String orderType, String code);
+    List<Order> findAllByOrderTypeAndCode(TransactionType orderType, String code);
 
-    List<Order> findAllByUserIdAndOrderTypeAndCode(Long userId, String orderType, String code);
+    List<Order> findAllByUserIdAndOrderTypeAndCode(Long userId, TransactionType orderType, String code);
 }
