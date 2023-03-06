28a826da1caf50cda33b4626ed0871c6b3a0d07d
NAM GIBEOM <rlqja0523@naver.com>
2023-03-06 14:10:12 +0900

    ♻️refactor: 타입 삭제

--- OneCoin/Server/src/main/java/OneCoin/Server/order/entity/enums/TransactionType.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/order/entity/enums/TransactionType.java
@@ -1,15 +1,8 @@
 package OneCoin.Server.order.entity.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
 public enum TransactionType {
-    ASK("ASK"),
-    BID("BID"),
-    DEPOSIT("DEPOSIT"),
-    SWAP("SWAP");
-
-    @Getter
-    private final String type;
+    ASK,
+    BID,
+    DEPOSIT,
+    SWAP
 }
