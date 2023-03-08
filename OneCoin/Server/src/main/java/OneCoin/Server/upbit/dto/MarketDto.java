e509d166ba3ed848277d89abc3fb9867b59f535f
NAM GIBEOM <rlqja0523@naver.com>
2023-03-08 12:14:22 +0900

    ♻️refactor: 필요없는 lombok 제거

--- OneCoin/Server/src/main/java/OneCoin/Server/upbit/dto/MarketDto.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/upbit/dto/MarketDto.java
@@ -3,16 +3,10 @@ package OneCoin.Server.upbit.dto;
 import OneCoin.Server.upbit.dto.orderbook.OrderBookDto;
 import OneCoin.Server.upbit.dto.ticker.TickerDto;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class MarketDto {
     private List<TickerDto> ticker;
     private List<OrderBookDto> orderBook;
