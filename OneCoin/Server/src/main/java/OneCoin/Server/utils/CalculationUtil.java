df2e18156b704fc1870d43d74a44a58e92788111
NAM GIBEOM <rlqja0523@naver.com>
2023-03-09 15:21:21 +0900

    📝docs: javadoc 추가

--- OneCoin/Server/src/main/java/OneCoin/Server/utils/CalculationUtil.java
+++ OneCoin/Server/src/main/java/OneCoin/Server/utils/CalculationUtil.java
@@ -8,6 +8,9 @@ import java.math.RoundingMode;
 
 @Component
 public class CalculationUtil {
+    /**
+     * 평균 단가를 계산한다.
+     */
     public BigDecimal calculateAvgPrice(BigDecimal holdingPrice, BigDecimal holdingAmount, BigDecimal newPrice, BigDecimal newAmount) {
         BigDecimal prevTotal = holdingPrice.multiply(holdingAmount);
         BigDecimal curTotal = newPrice.multiply(newAmount);
@@ -16,6 +19,9 @@ public class CalculationUtil {
         return prevTotal.add(curTotal).divide(totalAmount, 2, RoundingMode.HALF_UP);
     }
 
+    /**
+     * 전일 대비 상승률의 부호를 판별한다.
+     */
     public String calculateChangeRate(String price, String prevClosingPrice) {
         BigDecimal curPrice = new BigDecimal(price);
         BigDecimal prePrice = new BigDecimal(prevClosingPrice);
@@ -34,16 +40,27 @@ public class CalculationUtil {
         return "";
     }
 
+    /**
+     * 매도(ASK)
+     * 수수료를 제외한 총 금액을 계산한다.
+     */
     public BigDecimal calculateBySubtractingCommission(BigDecimal price, BigDecimal amount) {
         BigDecimal commissionRate = BigDecimal.ONE.subtract(Commission.ORDER.getRate());
         return price.multiply(amount).multiply(commissionRate);
     }
 
+    /**
+     * 매수(BID)
+     * 수수료를 더한 총 금액을 계산한다.
+     */
     public BigDecimal calculateByAddingCommission(BigDecimal price, BigDecimal amount) {
         BigDecimal commissionRate = BigDecimal.ONE.add(Commission.ORDER.getRate());
         return price.multiply(amount).multiply(commissionRate);
     }
 
+    /**
+     * 총 금액의 수수료를 계산한다.
+     */
     public BigDecimal calculateOrderCommission(BigDecimal price, BigDecimal amount) {
         return price.multiply(amount).multiply(Commission.ORDER.getRate()).setScale(2, RoundingMode.HALF_UP);
     }
